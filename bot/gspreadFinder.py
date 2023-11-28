469d087f16974340764c559eaa67ab5b3d38ecd0
Wonsang Kim <rosskysoss@gmail.com>
2023-11-28 16:29:35 +0900

    feat: random recommendation function

--- bot/gspreadFinder.py
+++ bot/gspreadFinder.py
@@ -2,11 +2,12 @@ from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 import os
 from dotenv import load_dotenv
+from recommendBot import Recommendation, OutputRestaurant
 
 load_dotenv()
 
 
-SERVICE_ACCOUNT_FILE = 'splendid-myth-353301-a63d721b9519.json'
+SERVICE_ACCOUNT_FILE = '../splendid-myth-353301-a63d721b9519.json'
 
 SPREADSHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 RANGE_NAME = '시트1!A2:G30'
@@ -20,8 +21,11 @@ sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
 values = result.get('values', [])
 
-if not values:
-    print('데이터를 찾을 수 없습니다.')
-else:
-    for row in values:
-        print(row)
+recommendation = Recommendation(values)
+output_restaurant = OutputRestaurant
+
+for rec_string in [OutputRestaurant(row.tolist()).__str__() for idx, row in recommendation.get_random(3).iterrows()]:
+    print(rec_string)
+
+print(recommendation.get_categorized_restaurant("한식"), 2)
+print(recommendation.get_close_restaurant(0.2, 2))
---
+++ bot/recommendBot.py
@@ -0,0 +1,39 @@
+import random
+import pandas as pd
+
+class OutputRestaurant:
+    def __init__(self, restaurant_info):
+        self.name = restaurant_info[0]
+        self.dist = restaurant_info[1]
+        self.category = restaurant_info[2]
+        self.menu = restaurant_info[3]
+        self.average_price = restaurant_info[4]
+        self.recommendation_score = restaurant_info[5]
+
+    def __str__(self):
+        return f"\"{self.name}\"을 추천드립니다. 거리{self.dist}km에 있는 {self.category}식당이고 " \
+               f"\n점심으로 {self.menu}가 먹을만 합니다. " \
+               f"\n평균 가격은 1인당 {self.average_price}," \
+               f"\n추천점수는 {self.recommendation_score}입니다.\n"
+
+
+class Recommendation:
+    def __init__(self, raw_data):
+        self.data_frame = pd.DataFrame(raw_data, columns=["가게명", "거리", "카테고리", "메뉴", "평균가격", "별점", "리뷰개수"])
+        self.data_frame["거리"] = self.data_frame["거리"].astype(float)
+        self.data_frame["평균가격"] = self.data_frame["평균가격"].astype(int)
+        self.data_frame["별점"] = self.data_frame["별점"].astype(float)
+
+    def get_random(self, num_rest=1):
+        return self.data_frame.sample(num_rest)
+
+    def get_categorized_restaurant(self, category, num_rest=1):
+        if category not in ["한식", "일식", "중식", "양식", "동남아식"]:
+            raise ValueError("카테고리는 '한식', '일식', '중식', '양식', '동남아식' 중에서 선택되어야 합니다.")
+        return self.data_frame[self.data_frame["카테고리"] == category].sample(num_rest)
+
+    def get_close_restaurant(self, min_dist, num_rest=1):
+        if not isinstance(min_dist, (int, float)):
+            raise TypeError("거리는 숫자형 이어야 함니다.")
+        return self.data_frame[self.data_frame["거리"] < min_dist].sample(num_rest)
+
